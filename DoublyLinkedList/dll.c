9fcb5d948d07ad7386c79df5d9ec975244ffbb62
Sujal <sjlchoudhari@gmail.com>
2023-10-27 12:23:29 +0000

    Doubly Linked List Code Updated

--- DoublyLinkedList/dll.c
+++ DoublyLinkedList/dll.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define nullptr 0x0
 
-struct Node
+typedef struct Node
 {
     int data;
     struct Node *prev;
     struct Node *next;
-};
-
-typedef struct Node Node;
+} Node;
 
 Node *createNode(int value)
 {
     Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->data = value;
-    newNode->prev = nullptr;
-    newNode->next = nullptr;
+    newNode->prev = NULL;
+    newNode->next = NULL;
     return newNode;
 }
 
 Node *insert_at_beginning(Node *head, int value)
 {
     Node *newNode = createNode(value);
-    if (head == nullptr)
+    if (head == NULL)
     {
         head = newNode;
     }
@@ -40,18 +37,17 @@ Node *insert_at_end(Node *head, int value)
 {
     Node *newNode = createNode(value);
 
-    if (head == nullptr)
+    if (head == NULL)
     {
         head = newNode;
     }
     else
     {
         Node *ptr = head;
-        while (ptr->next != nullptr)
+        while (ptr->next != NULL)
         {
             ptr = ptr->next;
         }
-        // got end element
         ptr->next = newNode;
         newNode->prev = ptr;
     }
@@ -59,52 +55,168 @@ Node *insert_at_end(Node *head, int value)
     return head;
 }
 
+Node *insert_at_position(Node *head, int value, int position)
+{
+    Node *newNode = createNode(value);
+    if (head == NULL && position > 1)
+    {
+        printf("Invalid position. List is currently empty.\n");
+        return head;
+    }
+    if (position == 1)
+    {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+    else
+    {
+        Node *ptr = head;
+        int count = 1;
+        while (count < position - 1 && ptr != NULL)
+        {
+            ptr = ptr->next;
+            count++;
+        }
+        if (ptr == NULL)
+        {
+            printf("Invalid position. The list is not long enough.\n");
+        }
+        else
+        {
+            newNode->next = ptr->next;
+            newNode->prev = ptr;
+            if (ptr->next != NULL)
+            {
+                ptr->next->prev = newNode;
+            }
+            ptr->next = newNode;
+        }
+    }
+    return head;
+}
+
+Node *delete_at_beginning(Node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty, nothing to delete.\n");
+    }
+    else
+    {
+        Node *temp = head;
+        head = head->next;
+        if (head != NULL)
+        {
+            head->prev = NULL;
+        }
+        free(temp);
+    }
+    return head;
+}
+
+Node *delete_at_end(Node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty, nothing to delete.\n");
+    }
+    else if (head->next == NULL)
+    {
+        free(head);
+        head = NULL;
+    }
+    else
+    {
+        Node *ptr = head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->prev->next = NULL;
+        free(ptr);
+    }
+    return head;
+}
+
+Node *delete_at_position(Node *head, int position)
+{
+    if (head == NULL)
+    {
+        printf("List is empty, nothing to delete.\n");
+    }
+    else if (position == 1)
+    {
+        head = delete_at_beginning(head);
+    }
+    else
+    {
+        Node *ptr = head;
+        int count = 1;
+        while (count < position && ptr != NULL)
+        {
+            ptr = ptr->next;
+            count++;
+        }
+        if (ptr == NULL)
+        {
+            printf("Invalid position. The list is not long enough.\n");
+        }
+        else if (ptr->next == NULL)
+        {
+            head = delete_at_end(head);
+        }
+        else
+        {
+            ptr->prev->next = ptr->next;
+            ptr->next->prev = ptr->prev;
+            free(ptr);
+        }
+    }
+    return head;
+}
+
 void display_dll(Node *head)
 {
     Node *ptr = head;
 
-    if (head == nullptr)
+    if (head == NULL)
     {
-        printf("\n\tLIST IS EMPTY!!!!!");
+        printf("\nThe List is Empty\n\n");
         return;
     }
 
-    while (ptr != nullptr)
+    printf("The List is: ");
+    while (ptr->next != NULL)
     {
-        printf("%d ", ptr->data);
+        printf("%d <-> ", ptr->data);
         ptr = ptr->next;
     }
-    printf("\n");
-}
+    printf("%d", ptr->data);
 
-Node* insert_at_position(Node* head,int value, int pos){
-    if(head == nullptr) {
-        printf("\n\tLIST IS EMPTY!!!!");
-        return;
-    }
-    if(pos == 1){
-        return insert_at_beginning(head,value);
-    }
-    else {
-        Node* ptr;
-        // while()
-    }
+    printf("\n");
 }
 
 void dll_menu()
 {
+    Node *head = NULL;
+
     while (1)
     {
-        Node *head;
-
         int choice = 0;
-        printf("Doubly Linked List:\n");
+        printf("\nDoubly Linked List:\n");
         printf("1. Display List\n");
         printf("2. Insert At Beginning\n");
         printf("3. Insert At End\n");
+        printf("4. Insert At Position\n");
+        printf("5. Delete At Beginning\n");
+        printf("6. Delete At End\n");
+        printf("7. Delete At Position\n");
+        printf("8. Exit\n");
 
         scanf("%d", &choice);
         int value = 0;
+        int position = 0;
         switch (choice)
         {
         case 1:
@@ -120,13 +232,35 @@ void dll_menu()
             scanf("%d", &value);
             head = insert_at_end(head, value);
             break;
-        default:
+        case 4:
+            printf("\nEnter value: ");
+            scanf("%d", &value);
+            printf("Enter position: ");
+            scanf("%d", &position);
+            head = insert_at_position(head, value, position);
+            break;
+        case 5:
+            head = delete_at_beginning(head);
+            break;
+        case 6:
+            head = delete_at_end(head);
+            break;
+        case 7:
+            printf("Enter position: ");
+            scanf("%d", &position);
+            head = delete_at_position(head, position);
+            break;
+        case 8:
             return;
+        default:
+            printf("Invalid choice. Please try again.\n");
+            break;
         }
     }
 }
 
-void main()
+int main()
 {
     dll_menu();
+    return 0;
 }
